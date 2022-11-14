c7124eae60f75c05c6425d99ae40d7df8b09e250
cena1499 <67260920+cena1499@users.noreply.github.com>
2022-11-14 22:46:47 +0100

    Easy CRUD operations

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -23,3 +23,77 @@ exports.register = async (req, res, next) => {
     });
   }
 };
+
+exports.login = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username, password });
+    if (!user) {
+      res.status(401).json({
+        message: "Login not successful",
+        error: "User not found",
+      });
+    } else {
+      res.status(200).json({
+        message: "Login successful",
+        user,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "An error occurred",
+      error: error.message,
+    });
+  }
+};
+
+exports.update = async (req, res, next) => {
+  const { role, id } = req.body;
+  // Verifying if role and id is presnt
+  if (role && id) {
+    // Verifying if the value of role is admin
+    if (role === "Admin") {
+      // Finds the user with the id
+      await User.findById(id)
+        .then((user) => {
+          // Third - Verifies the user is not an admin
+          if (user.role !== "Admin") {
+            user.role = role;
+            user.save((err) => {
+              //Monogodb error checker
+              if (err) {
+                res
+                  .status("400")
+                  .json({ message: "An error occurred", error: err.message });
+                process.exit(1);
+              }
+              res.status("201").json({ message: "Update successful", user });
+            });
+          } else {
+            res.status(400).json({ message: "User is already an Admin" });
+          }
+        })
+        .catch((error) => {
+          res
+            .status(400)
+            .json({ message: "An error occurred", error: error.message });
+        });
+    }
+  } else {
+    res.status(400).json({ message: "Role or Id not present" });
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  const { id } = req.body;
+  await User.findById(id)
+    .then((user) => user.remove())
+    .then((user) =>
+      res.status(201).json({ message: "User successfully deleted", user })
+    )
+    .catch((error) =>
+      res
+        .status(400)
+        .json({ message: "An error occurred", error: error.message })
+    );
+};
